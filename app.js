6c22ba1c9ab11d0090a047be429d466cf569fd96
MaianneThornton <maianne.thornton@gmail.com>
2021-11-01 12:14:42 -0400

    Added additional solutions in class

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 // 1a. Select the 1st button element and set it to a const variable called button1
-const button1 = document.querySelector(`button`);
+const button1 = document.querySelector(`#one`);
 
 // 1b. Using the ONCLICK event property, have an alert that says "Cool! You can hear/understand a hummingbird and ants" pop up when you click on the button
 button1.onclick = () => {
-  alert ('Cool! You can hear/understand a hummingbird and ants');
+  alert('Cool! You can hear/understand a hummingbird and ants');
 }
 
 // 2a. Select the h3 and set it to a const variable called h3
 const h3 = document.querySelector(`h3`);
 
 // 2b. Using addEventListener, have an alert that says "OH NO! The squirrels are on to you. Now you have to leave this reality and move to a new one" pop up when you hover over the h3
-h3.addEventListener (`mouseenter`, () => {
+h3.addEventListener(`mouseenter`, () => {
   alert(`OH NO! The squirrels are on to you. Now you have to leave this reality and move to a new one`);
 });
 
@@ -19,7 +19,53 @@ const form = document.querySelector(`form`);
 console.dir(form);
 
 // 3b. Using addEventListener, display the value in the text input with an alert when the submit input in the form is pressed.
+// form.addEventListener(`submit`, () => {
+//   const quote = form.elements.quote.value;
+//   alert(quote);
+// });
+
+// OR
 form.addEventListener(`submit`, () => {
-  const quote = form.elements.quote.value;
-  alert(quote);
+  alert(`${form.elements.quote.value}`);
 });
+
+// BONUS TERRITORY | BONUS TERRITORY
+// 4a. Select the Dark Mode button and set it to a const variable called darkMode
+const darkMode = document.getElementById(`dm`);
+
+// 4b. Using addEventListener, TOGGLE the "dark-mode" CSS class On/Off for EVERY element when the Dark Mode Button is clicked (HINT: You will need to use the asterisk "*" selector as well as a loop)
+const everything = document.querySelectorAll(`*`);
+darkMode.addEventListener(`click`, () => {
+  for (elements of everything) {
+    elements.classList.toggle(`dark-mode`);
+  }
+});
+
+// 5a. Select the "MOVE TO ANOTHER REALITY" button and set it to a const variable called reality
+const reality = document.getElementById(`reality`);
+
+// 5b. Using addEventListener, have an alert that says "You have successfully moved to another reality" pop up when you click on the button. However, have this setup so that when the button is clicked a third time have the following alert display instead of the "You have successfully moved to another reality" alert. New alert "OH NO! You can only move to a new another reality a couple times. You are stuck in this reality!". And finally, when the button is clicked a fourth, fifth, sixth, etc. time have it do nothing (AKA Disable the button).
+// reality.addEventListener(`click`, () => {
+//   for (i = 0; i <= 2; i++) {
+//     alert(`You have successfully moved to another reality`);
+
+//     for (i = 3; i <= 3; i++) {
+//       alert(`OH NO! You can only move to a new another reality a couple times. You are stuck in this reality!`);
+
+//       for (i = 4; i > 4; i++) {
+//         document.getElementById(`reality`).disabled = true;
+//       };
+//     };
+//   };
+// })
+
+let clickCount = 0;
+reality.addEventListener(`click`, () => {
+  clickCount++;
+  if (clickCount < 3){
+    alert(`You have successfully moved to another reality`); 
+  } else if (clickCount === 3){
+    alert(`OH NO! You can only move to a new another reality a couple times. You are stuck in this reality!`);
+  }
+});
+
